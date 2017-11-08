06758c6bc6c4520bd2a8a7edce27cfb783ecf021
Anna <anna.ruotanen@gmail.com>
2017-11-08 13:24:26 +0100

    Install FactoryGirl, Faker and define factories

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,29 +1,27 @@
-# spec/factories/rooms.rb
+#spec/factories/events.rb
 
-# FactoryGirl.define do
-#   factory :room do
-#     home_type         "House"
-#     room_type         "Shared"
-#     accommodate       2
-#     bedroom_count     2
-#     bathroom_count    3
-#     listing_name      { Faker::Lorem.words(3).join(' ') }
-#     description       { Faker::Lorem.sentence(40) }
-#     address           { Faker::Address.city }
-#     has_tv            true
-#     has_kitchen       true
-#     has_airco         true
-#     has_heating       true
-#     has_internet      true
-#     price             { Faker::Commerce.price }
-#     user              { build(:user) }
-#
-#     trait :active do
-#       active true
-#     end
-#
-#     trait :inactive do
-#       active false
-#     end
-#   end
-# end
+FactoryGirl.define do
+  factory :event do
+    event_name        "Blabla"
+    location          "Utrecht"
+    # price_count       20
+    capacity_count    30
+    listing_name      { Faker::Lorem.words(3).join(' ') }
+    description       { Faker::Lorem.sentence(40) }
+    address           { Faker::Address.city }
+    includes_food     true
+    includes_drinks   true
+    starts_at         13/10
+    ends_at           17/10
+    price             { Faker::Commerce.price }
+    user              { build(:user) }
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
