0e42927a726ab9a47ce5e2789d65ed7cb150014e
Anna <anna.ruotanen@gmail.com>
2017-11-08 20:33:32 +0100

    Add about page tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "about" => "pages#about"
 end
---
+++ spec/features/about_page_spec.rb
@@ -0,0 +1,12 @@
+# app/spec/features/about_page_spec.rb
+
+require "rails_helper"
+
+describe "About Page" do
+  it "shows information about the application" do
+    visit root_url
+    click_on "About"
+
+    expect(page).to have_content "This application was built in 2017"
+  end
+end
